f48fa81b3ddc047b1a8cd463d37b17fbad8976c1
Leonardo Murça <leonardoamurca@gmail.com>
2019-06-18 11:29:03 -0300

    Update endpoints

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,4 +1,1 @@
-export const login =
-  'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyD2Ct9oP9MpVDSzhqTYs8LzdKiQFSsSnSY';
-export const signUp =
-  'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyD2Ct9oP9MpVDSzhqTYs8LzdKiQFSsSnSY';
+
