2e57a9f293952a590bffc49449c48c46603372ab
Leonardo Murca <leonardoamurca@gmail.com>
2019-06-19 10:17:00 -0300

    gitignore

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,1 +1,3 @@
 
+export const loginUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyD2Ct9oP9MpVDSzhqTYs8LzdKiQFSsSnSY'
+export const signUpUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyD2Ct9oP9MpVDSzhqTYs8LzdKiQFSsSnSY'
