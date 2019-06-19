3faffd77b3d26d291cf887b2056cf2669299b567
Leonardo Murca <leonardoamurca@gmail.com>
2019-06-19 10:19:14 -0300

    Removing config

--- src/config/endpoints.js
+++
@@ -1,3 +0,0 @@
-
-export const loginUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyD2Ct9oP9MpVDSzhqTYs8LzdKiQFSsSnSY'
-export const signUpUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyD2Ct9oP9MpVDSzhqTYs8LzdKiQFSsSnSY'
